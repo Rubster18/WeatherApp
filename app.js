c843534e2c62e09a1eed1d632d102389ca2db883
Rubster <rubenadarme@gmail.com>
2020-05-22 16:15:29 +0200

    working weather javascript app

--- app.js
+++ app.js
@@ -1,48 +1,73 @@
-const apiKey = '416e0f0dd8c8e9042517b54f30bf565c';
-let latitude;
-let longitude;
-const notification = document.getElementsByClassName('notification')[0];
-let weather;
+const kelvinTemp = 273;
+const apiKey = "bd231dae6e6543c08e48120bb7214b0c";
 
-getLocation();
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }
+//selection of weather box items
+
+const boxNoti = document.querySelector(".notification");
+const boxIcon = document.querySelector(".weather-icon");
+const boxTemperature = document.querySelector(".temperature-value p");
+const boxDescrip = document.querySelector(".temperature-description p");
+const boxLocal = document.querySelector(".location p");
+
+
+
+const weather = {};
+
+weather.temperature = {
+    unit: "celcius"
 }
 
-function kelvinToCelsius(temp) {
-    return temp - 273.15;
+//user browser and/or denies
+if ('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, errorFunction);
+}else{
+    boxNoti.style.display ="block";
+    boxNoti.innerHTML = "<p> Browser doesn't use geolocation</p>";
 }
 
-function onSuccess(position) {
-    console.log(position);
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+//position 
 
-    const weatherCall = fetch('https://api.openweathermap.org/data/2.5/weather?'
-                            + 'lat=' + latitude
-                            + '&lon=' + longitude
-                            + '&appid=' + apiKey);
+function setPosition(position){
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-    weatherCall.then(response => response.json())
-            .then(weatherInfo => {
-                console.log(weatherInfo)
-                console.log(weatherInfo.weather[0].icon);
-                console.log(kelvinToCelsius(weatherInfo.main.temp).toFixed(1));
-                console.log(weatherInfo.weather[0].main);
-                console.log(weatherInfo.name);
-            });
+    getWeather(lat, lon);
 }
 
-function onError(error) {
-    console.error('No no no ', error);
-    // 1. take message and put it in a p
-    const p = document.createElement('p');
-    p.innerHTML = error.message;
-    // 2. display: block (notification div)
-    notification.style.display = 'block';
-    // 3. append p inside notification
-    notification.appendChild(p);
+//errors?
+
+function errorFunction(error){
+    boxNoti.style.display ="block";
+    boxNoti.innerHTML = `<p> ${error.message}</p>`;
 }
 
+//use api for weather object
+
+function getWeather(lat, lon) {
+    let api =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  /*  console.log(api); */
+  fetch(api)
+    .then(function(response){
+        let recieving = response.json();
+        return recieving; 
+    })
+    .then (function(recieving){
+        weather.temperature.value = Math.floor(recieving.main.temp - kelvinTemp); 
+        weather.description = recieving.weather[0].description;
+        weather.iconId = recieving.weather[0].icon;
+        weather.country = recieving.sys.country;
+        weather.city = recieving.name;
+    })
+    .then(function(){
+        displayWeather();
+    })
+}
+
+//changes in HTML
+
+function displayWeather(){
+    boxIcon.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    boxTemperature.innerHTML = `${weather.temperature.value}º<span>C</span>`;
+    boxDescrip.innerHTML = `<p>${weather.description}</p>`;
+    boxLocal.innerHTML = `<p>${weather.country}, ${weather.city}`;
+}
